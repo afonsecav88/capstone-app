02492f779142117988e8a0e0a4e6e161d2d5a7b1
Adrián Fonseca Vega <afonsecav88@gmail.com>
2024-04-29 12:13:23 -0300

    create footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,43 +1,46 @@
-import React from 'react'
+import React from 'react';
 import { Nav } from './Nav';
 
 export const Footer = () => {
   return (
     <footer className="footer-section">
-    <div>
-      <img
-        src="./Asset 18@4x.png"
-        alt="logo footer"
-        className="footer-img"
-      />
-    </div>
-    <div className='footer-doormat-navigator'>
-    <Nav/>
-    </div>
-    <div className='footer-contact'>
-    <ul className="py-5 items-center w-64 text-sm text-neutral-700">
-      <p className="font-bold">Dirección:</p>
-      <p>Calle del Sol, 123 Ciudad Fantasía, CP 12345 País del Sur</p>
+      <div>
+        <img
+          src="./Asset 18@4x.png"
+          alt="logo footer"
+          className="footer-logo-img"
+        />
+      </div>
+      <div className="footer-doormat-navigator">
+        <Nav />
+      </div>
+      <div className="footer-contact">
+        <p>Contact</p>
+        <ul className='footer-contact-list'>
+          <li><img src="/address.svg" alt="Address" /> <p>Calle del Sol, 123 Ciudad Fantasía, CP 12345 País del Sur</p></li>
+          <li> <img src="/phone.svg" alt="Phone" />  <p>+123 456 7890</p></li>
+          <li> <img src="/email.svg" alt="Mail" />  <p>info@littleLemon.com</p></li>
+        </ul>
+      </div>
+      <div className="footer-social-media">
+        <p>Social Media Links</p>
+        <ul className="footer-social-media-list">
+          <li>
+            <img src="/facebook.svg" alt="Facebook" /> @littleLemonrestaurante
+          </li>
+          <li>
+            <img src="/instagram.svg" alt="Instagram" />
+            @littleLemon_restaurante
+          </li>
+          <li>
+            <img src="/twitter.svg" alt="Twitter" /> @littleLemon_tw
+          </li>
+        </ul>
+      </div>
 
-      <p className="font-bold mt-5">Teléfono:</p>
-      <p>+123 456 7890</p>
-
-      <p className="font-bold mt-5">Correo Electrónico:</p>
-      <p>info@littleLemon.com</p>
-
-      <p className="font-bold mt-5">Redes Sociales:</p>
-      <li>Facebook: @littleLemonrestaurante</li>
-      <li>Instagram: @littleLemon_restaurante</li>
-      <li>Twitter: @littleLemon_tw</li>
-    </ul>
-    </div>
-    <div className='footer-social-media'>
-
-    </div>
-
-    {/* <div>
+      {/* <div>
       <section><p>&copy; Little Lemon Restaurant</p></section>
     </div> */}
-  </footer>
-  )
-}
+    </footer>
+  );
+};
